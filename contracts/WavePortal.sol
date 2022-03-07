08028f5ba724322ed1bba82aaa43ef12bb4af8ca
Kresna Sucandra <kresnasucandra@gmail.com>
2022-03-08 07:58:25 +0800

    Feat: Added contract wave/ getTotalWave function and test

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,8 +5,23 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a smart contract");
     }
+
+    // Wave function
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    // Function to get total wave number
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
 }
 
